577b9de4105cd5f70c67ce2e994249b23a5fb6ba
catenaccian <catenaccian@DESKTOP-LPNOHG5.localdomain>
2022-07-07 11:29:45 -0400

    modify code and settings for control and variable runs

--- source/analyze/AnalyzePangenome.hpp
+++ source/analyze/AnalyzePangenome.hpp
@@ -60,6 +60,8 @@ namespace mabe {
 
     void BeforeMutate(Organism & bit_org) override {
       // modify the organism & do the actual crossover
+      //when we want to do the probability test, all we need to change is to put a number in for the third parameter 
+      //string new_genome = pangenome_graph.modify_org(control.GetRandom(), bit_org.ToString(), 0.01);
       string new_genome = pangenome_graph.modify_org(control.GetRandom(), bit_org.ToString());
       bit_org.GenomeFromString(new_genome); //pass in organism reference and change its genome
     }
@@ -71,31 +73,6 @@ namespace mabe {
    void BeforeDeath(OrgPosition position) override {
       pangenome_graph.remove_sequence(position.OrgPtr()->ToString()); // remove_sequence in the graph before the organism dies
     }
-
-    int Evaluate(const Collection & orgs) {
-      // Loop through the population and calculate the number of unique genotypes.
-      emp::Ptr<Organism> max_org = nullptr;
-      mabe::Collection alive_orgs( orgs.GetAlive() );
-      
-      //define get_fun. no clue if this will work.
-      unique_genotypes = DataCollect::Unique<DATA_T, CONTAIN_T>(orgs, get_fun);
-
-      for (Organism & org : alive_orgs) {
-        //instead of all this fitness stuff, put the Unique function we talked about above
-        //DataCollect::Unique<DATA_T, CONTAIN_T>(container, get_fun);
-        //Unique(orgs, function that pulls out the bitvector for comparison)
-        org.SetTrait<int>(unique_genotypes);
-
-        //idk what the rest of this function is but modify accordingly
-        if (fitness > max_fitness || !max_org) {
-          max_fitness = fitness;
-          max_org = &org;
-        }
-      }
-
-      return max_fitness;
-    }
-
 
   };
 
