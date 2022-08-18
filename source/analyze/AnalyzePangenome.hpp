d810ba39c546593d4d22a1305b0c3febd5c94921
catenaccian <catenaccian@DESKTOP-LPNOHG5.localdomain>
2022-08-18 11:38:43 -0400

    get csv file describing the pangenomes graph when AnalyzePangenome module urns

--- source/analyze/AnalyzePangenome.hpp
+++ source/analyze/AnalyzePangenome.hpp
@@ -6,9 +6,19 @@
  *  @file  AnalyzePangenome.hpp
  *  @brief Crossover operator that tracks all existing genomes in order to randomly generate new genomes.
  * 
- * Things we are changing in expirimentation: P probability in modify_org, whether we have sequence count or no sequence count. 
- * Make these a double and a bool that you can put into the contructor. A member variable/trait that must be specified by the user of the module.
- * Will probably have to change tests after this is done.
+ *  ok so here's my problem: i would like to get more than one entry for each time step, with each edge that is in the graph.
+    not sure how to get it to enter 111->000 and then 111->101 for example, at the same time step. If this can be figured out,
+    then could also put in seq count for how many times 111 appears in the genome.
+    if i can write my own AddFunction then I could maybe make a loop for the "os<<" but 
+    could also just put all the datafile stuff in DBGraph, but then would just have one snap of that graph at one time
+    current idea: create multiple ostreams to append to as we iterate throguh the keys and adjs and slap into csv
+    data.Add(pangenome_graph.csv(), "Count", "number of times this sequence appears in the pangenome");
+    data.Add(pangenome_graph.csv(), "Adjacencies", "graph data");
+    data.Add(pangenome_graph.csv(), "From", "graph data");
+    data.Add(pangenome_graph.csv(), "To", "graph data");
+
+    OK SO go the csv without using datafile and just ofstream, but it's only for one graph at one time
+    can i pass the file back and forth so i keep appending to the same one at each time
  */
 
 #ifndef MABE_ANALYZE_PANGENOME_HPP
@@ -24,9 +34,18 @@ namespace mabe {
 
   class AnalyzePangenome : public Module {
   private:
-    double probability; // Probability that the genome of an organism is modified with the DBGraph before it mutates.
-    bool count_kmers; // If true, a kmer in the DBGraph can only be used the same number of times it appears in the pangenome.
-    DeBruijnGraph pangenome_graph;
+  /// Function of module.
+    double probability; // Probability that the genome of an organism is modified with the DBGraph before it mutates
+    bool count_kmers; // If true, a kmer in the DBGraph can only be used the same number of times it appears in the pangenome
+    DeBruijnGraph pangenome_graph; // DeBruijn Graph storing pangenome
+
+  /// Datafile variables
+    emp::DataFile data; // The data file object
+  
+  /// Variables to add to datafile to describe DeBruijn graph.
+    int count; // Number of times this sequence (kmer) appears in the pangenome
+    string from; // ID of a kmer/sequence/node
+    string to; // ID to an adjacency (represents a directed edge)
 
   public:
     AnalyzePangenome(mabe::MABE & control,
@@ -35,6 +54,7 @@ namespace mabe {
           double _probability=1, bool _count_kmers=1)
       : Module(control, name, desc)
       , probability(_probability), count_kmers(_count_kmers)
+      , data(""), count(0), from(""), to("")
     {
       SetAnalyzeMod(true);
     }
@@ -48,9 +68,59 @@ namespace mabe {
       LinkVar(count_kmers, "count_kmers", "If true, a kmer in the DBGraph can only be used the same number of times it appears in the pangenome.");
     }
 
-    void SetupModule() override {    }
+    void SetupModule() override {
+      data = emp::DataFile("DeBruijnGraph.csv");
 
-    void OnInjectReady(Organism & bit_org, Population & pop) override {
+      std::function<size_t ()> updatefun = [this](){return control.GetUpdate();};
+      data.AddFun(updatefun,"Time", "The current time step/generation");
+      
+      data.AddFun<int>([this]() 
+      {
+        tuple<int, int> tup = pangenome_graph.kmer_count(count, from, to);
+        count = std::get<1>(tup);
+        return std::get<0>(tup);
+      }, "Count", "Number of times a kmer appears in the entire pangenome");
+     // seq count runs, we have updated seq count, and old from and old to.
+
+      data.AddFun<string>([this]()
+      {
+        tuple<string, string> tup = pangenome_graph.from(count, from, to);
+        from = std::get<1>(tup);
+        return std::get<0>(tup);
+      }, "From", "A kmer node on the graph");
+      // from runs, we have updated seq count and from, and old to. 
+
+      data.AddFun<string>([this]()
+      {
+        tuple<string, string> tup = pangenome_graph.to(count, from, to);
+        to = std::get<1>(tup);
+        return std::get<0>(tup);
+      }, "To", "An adjacent kmer");
+      // seq count runs, we have updated seq count and old from and to.
+
+      data.PrintHeaderKeys();
+    }
+
+    void OnUpdate(size_t update) override {
+      if(update%100==0){
+        /*for(auto i:pangenome_graph.get_all_vertices()){
+        cout<<i<<" adj_list_sz= "<<pangenome_graph.get_value(i).adj_list_size()<<" kmer_count= "<<pangenome_graph.get_value(i).get_kmer_occurrences()<<" -> ";
+        for(auto j:pangenome_graph.get_value(i).get_adj_list()){
+          cout<<j<<" ";
+        }
+        cout<<endl;}*/
+        tuple<int, string, string> tup = pangenome_graph.csv_start_values();
+        count = std::get<0>(tup);
+        from = std::get<1>(tup);
+        to = std::get<2>(tup);
+        for(int i = 0; i < pangenome_graph.edge_count()-1; ++i){
+          data.Update(); //update 4 vars as many times as we need to to go through all graph vertices
+        }
+        pangenome_graph.reset_vertex_flags();
+      }      
+    }
+
+    void BeforePlacement(Organism & bit_org, OrgPosition pos, OrgPosition parent_pos) override { //should i use this or on offspring ready?
       pangenome_graph.add_sequence(bit_org.ToString());
     }
 
@@ -65,12 +135,8 @@ namespace mabe {
       }
       bit_org.GenomeFromString(new_genome); //pass in organism reference and change its genome
     }
-    
-   void OnMutate(Organism & bit_org) override {
-      pangenome_graph.add_sequence(bit_org.ToString()); // add_sequence of the organism to the graph
-   }
 
-   void BeforeDeath(OrgPosition position) override {
+    void BeforeDeath(OrgPosition position) override {
       pangenome_graph.remove_sequence(position.OrgPtr()->ToString()); // remove_sequence in the graph before the organism dies
     }
 
